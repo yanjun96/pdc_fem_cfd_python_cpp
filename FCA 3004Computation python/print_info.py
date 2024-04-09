4ad8349d45ca111a5599299b62bb4bd461791d95
Yanjun96 <simpson1996@live.cn>
2024-04-09 14:40:47 +0200

    daily practice

---
+++ FCA 3004Computation python/print_info.py
@@ -0,0 +1,16 @@
+# print_infor.py
+
+def print_info():
+    my_dict = {
+    "name":"yanjun",
+    "age" :28,
+    "city":"Stockholm"
+    }
+    for i,k in my_dict.items():
+        print(i,k)
+    return my_dict.items()
+
+
+def yanjun():
+    print("yanjun is so cool!")
+    return "yanjun is so cool!"

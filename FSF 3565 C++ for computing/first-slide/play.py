656a858982f38b60fcb933f1dc42fe98cc6b8250
Yanjun96 <simpson1996@live.cn>
2024-06-16 19:09:57 +0200

    play

---
+++ FSF 3565 C++ for computing/first-slide/play.py
@@ -0,0 +1,6 @@
+a = int (input("Please enter your yearly salary, unit is W(rmb)"))
+if a >= 10:
+   print(f"Your salary {a}W rmb is above the average")
+else:
+   print(f"Your salary is {a}, your are a looser")
+

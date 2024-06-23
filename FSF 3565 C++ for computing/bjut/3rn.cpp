6c4bcfc40653b69841d1d732120dd2f29087f4d0
Yanjun96 <simpson1996@live.cn>
2024-06-23 11:27:11 +0200

    Bjut lectures

---
+++ FSF 3565 C++ for computing/bjut/3rn.cpp
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+
+{
+int month,day;
+printf("PLease enter your birthday, month day");
+scanf("%d%d",&month,&day);
+printf("Your birthday is %d, %d",month,day);
+return 0;
+}

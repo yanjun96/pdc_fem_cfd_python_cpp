43cc86a61b1dffdb0bb11c2338cf37889769eba4
Yanjun96 <simpson1996@live.cn>
2024-07-15 16:50:07 +0200

    daily

--- cpp_bjut/code/function/prime1.cpp
+++ cpp_bjut/code/function/prime1.cpp
@@ -2,27 +2,32 @@
 #include<math.h>
 int prime(int m);
 int main() {
-
 int i;
 
-for (i=2; i<=100; i++){
+for (i=0; i<=100; i++){
   if (i%10 == 0){
-  printf("\n");
+      printf("\n");
   }
   if (prime(i) == 1) { 
-  printf("%8d",i);
+  printf("%6d",i);
+  }
+  if (prime(i) == 0) {
+  printf("      ");
   }
 }
-
 return 0;
 }
 
 
 int prime(int m){
 int i;
+
 if (m == 1){ 
 return 0;
 }
+
+if (m == 0){
+return 0;}
 
 for (i=2; i<=sqrt(m); i++){
   if (m%i == 0){

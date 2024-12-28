45b269d7801da8d2f829024678727ceba1daea5a
yanjun zhang <simpson1996@live.cn>
2024-12-28 22:33:39 +0100

    with mac

---
+++ fun/1e6.py
@@ -0,0 +1,3 @@
+c = int(5e6)
+for i in range(c):
+  print(i)
---
+++ fun/ce6.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+
+{
+
+int i, c;
+c = 5e6;
+i = 1;
+
+for (i=0; i<= c; i++) {
+std::cout << i << "  ";
+}
+return 0;
+}

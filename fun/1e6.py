9ac5c0b4700c6366ecdf967bf91aedb2319660f2
yanjun zhang <simpson1996@live.cn>
2024-12-28 22:41:58 +0100

    concise the structure

--- fun/1e6.py
+++
@@ -1,3 +0,0 @@
-c = int(5e6)
-for i in range(c):
-  print(i)
--- fun/python.py
+++
@@ -1,8 +0,0 @@
-import time
-start_time = time.time()
-c = int(1e7)
-for i in range(c):
-    print(i)
-end_time = time.time()
-during = end_time - start_time
-print("Time taken",during)

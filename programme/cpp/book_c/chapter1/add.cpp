04ff81e39cf5606a230ad39426ccc2b40190d481
yanjun zhang <simpson1996@live.cn>
2025-03-21 09:59:33 +0100

    learning cahr and float in cpp

---
+++ programme/cpp/book_c/chapter1/add.cpp
@@ -0,0 +1,15 @@
+int add(int a, int b){
+   int c;
+   c = a + b;
+   return c;
+}
+
+#include <cstdio>
+int main(){
+int a = 2;
+int b = 3;
+int c;
+c = add(a,b);
+printf("%d\n",c);
+}
+
---
+++ programme/cpp/book_c/chapter1/decima.cpp
@@ -0,0 +1,7 @@
+#include<cstdio>
+
+int main(){
+float a;
+a = 9.955;
+printf("a is %f.\n",a);
+}
